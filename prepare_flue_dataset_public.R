c106ca571020fba6b50b2c17752923fb76ace309
stineb <benjamin.stocker@gmail.com>
2018-02-20 13:47:40 +0100

    Added missing files and updated knit file.

--- prepare_flue_dataset_public.R
+++ prepare_flue_dataset_public.R
@@ -20,3 +20,15 @@ out <- nice_agg %>% select( mysitename, year, doy, fvar, is_drought_byvar ) %>%
                     select( site=mysitename, date, year, doy, flue=fvar, is_flue_drought=is_drought_byvar, cluster )
 
 write_csv( out, path="data/flue_stocker18nphyt.csv" )
+
+
+# Description:
+
+# This dataset contains fLUE data as described in Stocker et al., 2018 (New Phytologist). fLUE is derived from the FLUXNET 2015 dataset, Tier 1, daily. Only sites are included where the method for quantifying fLUE satisfied performance criteria (see Stocker et al. 2018). 
+# site: Site name (ID) from the FLUXNET network
+# date: DD/MM/YY
+# year: year
+# doy: day of year
+# fLUE: unitless, fraction of actual over potential light use efficiency, derived from artificial neural networks. This quantifies the fractional reduction in light use efficiency due to soil moisture (1 = no reduction).
+# is_flue_drought: TRUE if the data is identified as a 'drought' based on deviation of fLUE from 1 (see Stocker et al., 2018)
+# cluster: sites are assigned to clusters based on their typical parallel evolution of greenness and fLUE throughout drought events. cDD: 'drought deciduous', cGR: 'evergreen', cLS: 'low sensitivity', cNA: 'not affected'. 
